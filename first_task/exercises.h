d1d6feaa97f85cbbb87e5aa321c0c12b01c02194
deyby01 <camachodeybygerman@gmail.com>
2026-03-21 13:44:28 -0300

    feat: The `Strength` and `Cardio` classes have been implemented, along with the `getter` and `setter` methods.
    Class and method declarations in `exercises.h` are currently only
    partially complete.

--- first_task/exercises.h
+++ first_task/exercises.h
@@ -14,13 +14,53 @@ class Exercise{
     public:
         Exercise();
         Exercise(int id, string name, string type, string intensity_level, double estimated_time, string description, string last_used);
-        ~Exercise();
+        virtual ~Exercise();
+
+        // Getter's and setter's
+        int getId() const;
+        void setId(int id);
+
+        string getName() const;
+        void setName(string name);
+
+        string getType() const;
+        void setType(string type);
+
+        string getIntensityLevel() const;
+        void setIntensityLevel(string intensity_level);
+
+        double getEstimatedTime() const;
+        void setEstimatedTime(double estimated_time);
+
+        string getDescription() const;
+        void setDescription(string description);
+
+        string getLastUsed() const;
+        void setLastUsed(string last_used);
 
         void create();
         void update();
         void deleteExercise();
-        void showDetail() const;
-        void search();
-        void generateRoute();
+        virtual void showDetail() const;
 
+};
+
+class Strength: public Exercise {
+    public:
+        Strength();
+        Strength(int id, string name, string intensity_level, double estimated_time,
+                string description, string last_used);
+        ~Strength();
+
+        void showDetail() const override;
+};
+
+class Cardio: public Exercise {
+    public:
+        Cardio();
+        Cardio(int id, string name, string intensity_level, double estimated_time,
+              string description, string last_used);
+        ~Cardio();
+
+        void showDetail() const override;
 };
