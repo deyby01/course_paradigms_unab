d6e294a8611807b90da9d9efd06793e82494fb4f
rxbertx01 <varillas19@gmail.com>
2026-03-24 18:01:43 -0300

    feat: implement create exercise option

--- first_task/main.cpp
+++ first_task/main.cpp
@@ -1,5 +1,6 @@
 #include "exercises.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void menu() {
@@ -13,25 +14,66 @@ void menu() {
     cout << "7. Exit" << endl;
 }
 
-int main (int argc, char *argv[]) {
+int main () {
 
+    vector<Exercise*> exercises;
     // Strength e1(1, "Mechanic stair", "Beginner", 30.0, "For burn calories", "yestarday");
     // e1.showDetail();
 
     // If you want to test basic functionality coment block of white and
     // uncomment and run the code above.
+
     while (true){
         cout << endl;
         menu();
+
         int choice;
         cout << "Enter your choice: ";
         cin >> choice;
 
-        if (choice != 7) {
-            cout << "comming soon." << endl;
-        } else {
+        if (choice == 1) {
+
+            int tipo;
+            cout << "\nType of exercise:" << endl;
+            cout << "1. Strength" << endl;
+            cout << "2. Cardio" << endl;
+            cout << "Option: ";
+            cin >> tipo;
+
+            cin.ignore();
+
+            Exercise* nuevo;
+
+            if (tipo == 1) {
+                nuevo = new Strength();
+            } 
+            else if (tipo == 2) {
+                nuevo = new Cardio();
+            } 
+            else {
+                cout << "Invalid option." << endl;
+                continue;
+            }
+
+            // Crear ejercicio
+            nuevo->create();
+
+            // Guardar
+            exercises.push_back(nuevo);
+
+            // Verificación
+            cout << "\nExercise created successfully:\n";
+            nuevo->showDetail();
+        }
+
+        else if (choice == 7) {
             break;
         }
+
+        else {
+            cout << "Option not available." << endl;
+        }
     }
+
     return 0;
 }
